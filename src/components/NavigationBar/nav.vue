80c93c295f8343c13f0a62e592f0baa5d8d98f05
Nathan Lively <ntlively@ufl.edu>
2019-08-14 20:01:39 -0400

    BootstrapVUE Added and Working
    Merging Project Back To Master . https://stackoverflow.com/questions/49371467/bootstrap-vue-doesnt-load-css

--- src/components/NavigationBar/nav.vue
+++ src/components/NavigationBar/nav.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-brand href="#">Venitox</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
+        <!-- Create A NAV ITEM for each route we have registered -->
+        <b-nav-item v-for="routes in links" 
+            v-bind:key="routes.id"
+            :to="`${routes.page}`">{{routes.text}}
+        </b-nav-item>
+     </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
@@ -18,31 +21,10 @@
           <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template slot="button-content"><em>User</em></template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </div>
-  <!-- <div>
-    <h2>Nav Bar</h2>
-    <nav>
-      <router-link v-for="routes in links" 
-      v-bind:key="routes.id"
-      :to="`${routes.page}`" class="spacing">{{routes.text}}</router-link>
-    </nav>
-  </div> -->
 </template>
 
 <script>
